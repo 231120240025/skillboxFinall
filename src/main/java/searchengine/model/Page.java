ecced1afbf85fb8416867d255af605818b5f59b1
8NEMO <accidentxxxx@gmail.com>
2024-11-29 01:50:35 +0500

    Создайте в проекте папку model и в ней — классы, которые будут соответствовать таблицам site и page в базе данных. Структура таблиц описана в технической спецификации. Создайте их по правилам, которые вы изучали в модуле курса «Работа с MySQL в Java». В частности, не забудьте про аннотации @Entity, @Id, @GeneratedValue, @ManyToOne и @JoinColumn. Для enum-поля создайте отдельный Enum (его можно поместить в ту же папку model). Различные типы текстовых полей обозначайте аннотацией @Column:
    @Column(columnDefinition = "ENUM('INDEXING', 'INDEXED')")
    @Column(columnDefinition = "VARCHAR(255)")
    @Column(columnDefinition = "TEXT")
    @Column(columnDefinition = "MEDIUMTEXT")
    
    После создания классов для таблиц базы данных запустите приложение и убедитесь, что в базе данных появились соответствующие таблицы и что они имеют верную структуру.
    
    Обратите внимание, что по полю path в таблице page должен быть установлен индекс, чтобы поиск по нему был быстрым, когда в нём много ссылок. Узнать об индексах можно в курсе Язык запросов SQL.

---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,34 @@
+package searchengine.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name = "page",
+        indexes = @Index(name = "idx_path", columnList = "path")
+)
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site site;
+
+    @Column(length = 255, nullable = false)
+    private String path;
+
+    @Column(nullable = false)
+    private Integer code;
+
+    @Column(columnDefinition = "MEDIUMTEXT", nullable = false)
+    private String content;
+}
---
+++ src/main/java/searchengine/model/Site.java
@@ -0,0 +1,36 @@
+package searchengine.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "site")
+public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "ENUM('INDEXING', 'INDEXED', 'FAILED')", nullable = false)
+    private Status status;
+
+    @Column(name = "status_time", nullable = false)
+    private LocalDateTime statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(nullable = false, length = 255)
+    private String url;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+}
